8aa9c0e52498d0ac9536eda40928d5a7bdd597f4
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:28:27 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/Experience.java
+++ src/main/java/com/example/resumecreate/domains/Experience.java
@@ -1,9 +1,27 @@
 package com.example.resumecreate.domains;
 
+import lombok.*;
+
+import javax.persistence.*;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/11/22 23:46 (Thursday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Experience {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String date;
+    private String company;
+    @Column(columnDefinition = "text")
+    private String description;
 }
