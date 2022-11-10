c149c046c1d179b113ef48bd44faf6018b8922e9
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:25:23 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/Education.java
+++ src/main/java/com/example/resumecreate/domains/Education.java
@@ -1,9 +1,26 @@
 package com.example.resumecreate.domains;
 
+import lombok.*;
+
+import javax.persistence.*;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/11/22 23:29 (Thursday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Education {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String date;
+    private String name;
+    @Column(columnDefinition = "text")
+    private String description;
 }
