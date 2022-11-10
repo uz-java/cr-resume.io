cca15cc383c290c7259b0fe4b591ff186725c73d
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:22:23 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/Skill.java
+++ src/main/java/com/example/resumecreate/domains/Skill.java
@@ -1,9 +1,27 @@
 package com.example.resumecreate.domains;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:32 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private byte progress;
 }
