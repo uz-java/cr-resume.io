f932c200c0a319ece10163adcd2fe1950fc19250
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:15:28 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/SocialMediaAccount.java
+++ src/main/java/com/example/resumecreate/domains/SocialMediaAccount.java
@@ -1,9 +1,16 @@
 package com.example.resumecreate.domains;
 
+import lombok.*;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:33 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class SocialMediaAccount {
 }
