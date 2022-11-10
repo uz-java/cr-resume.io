1a0e7072a1874e1c204627e5a018f9ebc93ac43f
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:37:20 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/Picture.java
+++ src/main/java/com/example/resumecreate/domains/Picture.java
@@ -1,9 +1,30 @@
 package com.example.resumecreate.domains;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:32 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Picture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String path;
+    private String originalName;
+    private String generatedName;
+    private long size;
+    private String contentType;
 }
