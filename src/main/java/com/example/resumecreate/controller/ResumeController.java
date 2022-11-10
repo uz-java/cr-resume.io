73f638d401e012d87f583fc00b6e80cf33c5c43d
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 03:04:17 +0500

    commit

--- src/main/java/com/example/resumecreate/controller/ResumeController.java
+++ src/main/java/com/example/resumecreate/controller/ResumeController.java
@@ -1,9 +1,37 @@
 package com.example.resumecreate.controller;
 
+import com.example.resumecreate.domains.Picture;
+import com.example.resumecreate.dtos.resume.ResumeCreateDTO;
+import com.example.resumecreate.dtos.resume.ResumeDTO;
+import com.example.resumecreate.service.resume.ResumeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/11/22 23:30 (Thursday)
  * ResumeCreate/IntelliJ IDEA
  */
+@RestController
+@RequestMapping("/resume")
+@RequiredArgsConstructor
 public class ResumeController {
+    private final ResumeService service;
+
+    @PostMapping(value = "/create", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<ResumeDTO> generateResume(@RequestBody ResumeCreateDTO dto) {
+        ResumeDTO response = service.create(dto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/uploadPicture",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Picture> uploadPicture(@RequestParam("file")MultipartFile file,@RequestParam Long resumeId){
+        Picture picture=service.attachPicture(file,resumeId);
+        return new ResponseEntity<>(picture,HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/example/resumecreate/service/resume/ResumeService.java
+++ src/main/java/com/example/resumecreate/service/resume/ResumeService.java
@@ -1,9 +1,19 @@
 package com.example.resumecreate.service.resume;
 
+import com.example.resumecreate.domains.Picture;
+import com.example.resumecreate.dtos.resume.ResumeCreateDTO;
+import com.example.resumecreate.dtos.resume.ResumeDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:55 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Service
 public interface ResumeService {
+    ResumeDTO create(ResumeCreateDTO dto);
+
+    Picture attachPicture(MultipartFile file, Long resumeId);
 }
