f46c6440361901d4c9b09d33efa4f826ff2269f3
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 04:44:12 +0500

    commit

--- src/main/java/com/example/resumecreate/service/resume/ResumeServiceImpl.java
+++ src/main/java/com/example/resumecreate/service/resume/ResumeServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.resumecreate.service.resume;
 
 import com.example.resumecreate.domains.Picture;
+import com.example.resumecreate.domains.Resume;
 import com.example.resumecreate.dtos.resume.ResumeCreateDTO;
 import com.example.resumecreate.dtos.resume.ResumeDTO;
+import com.example.resumecreate.mappers.ResumeMapper;
+import com.example.resumecreate.repository.ResumeRepository;
+import com.example.resumecreate.service.file.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,18 +19,29 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class ResumeServiceImpl implements ResumeService{
+    private final ResumeMapper resumeMapper;
+    private final ResumeRepository repository;
+    private final FileService fileService;
     @Override
     public ResumeDTO create(ResumeCreateDTO dto) {
-        return null;
+        Resume resume = resumeMapper.fromCreateDTO(dto);
+        Resume save = repository.save(resume);
+        ResumeDTO resumeDTO = resumeMapper.fromResume(save);
+        return resumeDTO;
     }
 
     @Override
     public Picture attachPicture(MultipartFile file, Long resumeId) {
-        return null;
+        Picture picture = fileService.uploadPicture(file);
+        Resume resume = repository.findById(resumeId).orElseThrow(() -> new RuntimeException("Resume not found id: " + resumeId));
+        resume.setPicture(picture);
+        repository.save(resume);
+        return picture;
     }
 
     @Override
     public byte[] generateAsPDF(Long resumeId) {
+
         return new byte[0];
     }
 }
