e2924937db52aa75217dabf77cc3ad96d2f05857
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 02:16:32 +0500

    commit

--- src/main/java/com/example/resumecreate/configs/FreemarkerConfigurer.java
+++ src/main/java/com/example/resumecreate/configs/FreemarkerConfigurer.java
@@ -1,9 +1,21 @@
 package com.example.resumecreate.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/11/22 23:31 (Thursday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Configuration
 public class FreemarkerConfigurer {
+
+    @Bean
+    public FreeMarkerConfigurationFactoryBean configuration(){
+        FreeMarkerConfigurationFactoryBean bean=new FreeMarkerConfigurationFactoryBean();
+        bean.setTemplateLoaderPath("classpath:/templates/");
+        return bean;
+    }
 }
