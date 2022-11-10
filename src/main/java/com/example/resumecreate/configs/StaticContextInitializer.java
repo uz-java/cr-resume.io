3d343f7fd343cd393adcd198a2c854868f4e788f
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 02:26:08 +0500

    commit

--- src/main/java/com/example/resumecreate/configs/StaticContextInitializer.java
+++ src/main/java/com/example/resumecreate/configs/StaticContextInitializer.java
@@ -1,9 +1,24 @@
 package com.example.resumecreate.configs;
 
+import com.example.resumecreate.utils.TemplateUtils;
+import freemarker.template.Configuration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:38 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Component
+@RequiredArgsConstructor
 public class StaticContextInitializer {
+    private final Configuration configuration;
+
+    @PostConstruct
+    public void init(){
+        TemplateUtils.setConfiguration(configuration);
+    }
 }
--- src/main/java/com/example/resumecreate/utils/TemplateUtils.java
+++ src/main/java/com/example/resumecreate/utils/TemplateUtils.java
@@ -1,9 +1,33 @@
 package com.example.resumecreate.utils;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.IOException;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:55 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
 public class TemplateUtils {
+    private static Configuration configuration;
+
+    public TemplateUtils() {
+    }
+
+    public static void setConfiguration(Configuration configuration){
+        TemplateUtils.configuration=configuration;
+    }
+
+    public static Template getTemplate(String templateName){
+        try {
+            if (!templateName.endsWith(".ftl")){
+                templateName=templateName+".ftl";
+            }
+            return configuration.getTemplate(templateName);
+        }catch (IOException e){
+            throw new RuntimeException("Template not found");
+        }
+    }
 }
