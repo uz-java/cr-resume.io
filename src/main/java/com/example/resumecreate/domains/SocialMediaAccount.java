0415d1f5b16372f0ce8af411ccc5a18e19907e3d
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:19:45 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/SocialMediaAccount.java
+++ src/main/java/com/example/resumecreate/domains/SocialMediaAccount.java
@@ -2,6 +2,11 @@ package com.example.resumecreate.domains;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:33 (Friday)
@@ -12,5 +17,12 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
+@Entity
 public class SocialMediaAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String url;
 }
