8c8c64c460331b6c2301520fe7b1d36830b6511d
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 02:01:31 +0500

    commit

--- src/main/java/com/example/resumecreate/mappers/ResumeMapper.java
+++ src/main/java/com/example/resumecreate/mappers/ResumeMapper.java
@@ -1,9 +1,17 @@
 package com.example.resumecreate.mappers;
 
+import com.example.resumecreate.domains.Resume;
+import com.example.resumecreate.dtos.resume.ResumeCreateDTO;
+import com.example.resumecreate.dtos.resume.ResumeDTO;
+import org.mapstruct.Mapper;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:52 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Mapper(componentModel = "spring")
 public interface ResumeMapper {
+    Resume fromCreateDTO(ResumeCreateDTO dto);
+    ResumeDTO fromResume(Resume resume);
 }
--- src/main/java/com/example/resumecreate/repository/ResumeRepository.java
+++ src/main/java/com/example/resumecreate/repository/ResumeRepository.java
@@ -1,9 +1,12 @@
 package com.example.resumecreate.repository;
 
+import com.example.resumecreate.domains.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 11/11/22 00:52 (Friday)
  * ResumeCreate/IntelliJ IDEA
  */
-public interface ResumeRepository {
+public interface ResumeRepository extends JpaRepository<Resume,Long> {
 }
