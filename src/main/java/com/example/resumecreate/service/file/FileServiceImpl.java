f78d4390f20a4943709f69a2c2b3a25e869afc42
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 04:35:25 +0500

    commit

--- src/main/java/com/example/resumecreate/service/file/FileServiceImpl.java
+++ src/main/java/com/example/resumecreate/service/file/FileServiceImpl.java
@@ -3,7 +3,13 @@ package com.example.resumecreate.service.file;
 import com.example.resumecreate.domains.Picture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 /**
  * @author "Tojaliyev Asliddin"
@@ -13,8 +19,30 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class FileServiceImpl implements FileService{
+    private static Path rootPath=Path.of("/home/asliddin/uploads");
     @Override
     public Picture uploadPicture(MultipartFile picture) {
-        return null;
+        try {
+            String contentType= picture.getContentType();
+            String originalFilename = picture.getOriginalFilename();
+            long size = picture.getSize();
+            String filename = StringUtils.getFilename(originalFilename);
+            String filenameExtension = StringUtils.getFilenameExtension(originalFilename);
+            String generatedName=System.currentTimeMillis()+"."+filenameExtension;
+            Path uploadPath = rootPath.resolve(generatedName);
+            Picture uploads=Picture
+                    .builder()
+                    .contentType(contentType)
+                    .originalName(filename)
+                    .size(size)
+                    .generatedName(generatedName)
+                    .path(uploadPath.toString())
+                    .build();
+            Files.copy(picture.getInputStream(),uploadPath, StandardCopyOption.REPLACE_EXISTING);
+            return uploads;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Something wrong try again");
+        }
     }
 }
