962543ddd4f01c9ca8352eb343dbd57490a34c0e
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:34:48 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/Language.java
+++ src/main/java/com/example/resumecreate/domains/Language.java
@@ -1,9 +1,28 @@
 package com.example.resumecreate.domains;
 
+import com.example.resumecreate.enums.Proficiency;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/11/22 23:47 (Thursday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Proficiency proficiency;
 }
