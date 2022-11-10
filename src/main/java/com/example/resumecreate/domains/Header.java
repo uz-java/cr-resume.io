ceface76363fcdb0fc1a5a4323bcffde9f4485ba
asliddin <asliddintojaliyev88@gmail.com>
2022-11-11 01:31:49 +0500

    commit

--- src/main/java/com/example/resumecreate/domains/Header.java
+++ src/main/java/com/example/resumecreate/domains/Header.java
@@ -1,9 +1,32 @@
 package com.example.resumecreate.domains;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 10/11/22 23:47 (Thursday)
  * ResumeCreate/IntelliJ IDEA
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Header {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String address;
+    private String country;
+    private String job;
+    private String phoneNumber;
+    private String email;
+    private String site;
 }
