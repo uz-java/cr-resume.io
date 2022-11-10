2e65de73042286271349b308a21b0242119df077
asliddin <asliddintojaliyev88@gmail.com>
2022-11-10 23:07:49 +0500

    commit

---
+++ src/main/java/com/example/resumecreate/ResumeCreateApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumecreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeCreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeCreateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resumecreate/ResumeCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumecreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeCreateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
